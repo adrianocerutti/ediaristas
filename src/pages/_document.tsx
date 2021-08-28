1779556165c40a6d09b610bc7b273458d026d3ba
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-08-28 12:24:21 -0300

    adicionada fonte ao arquivo globals.css e link da fonte treinaweb ao arquivo _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
 
 	render(): JSX.Element {
 		return (
-			<Html>
+			<Html lang="pt_BR">
 				<Head>
 					<link
 						rel="stylesheet"
@@ -52,6 +52,10 @@ export default class MyDocument extends Document {
 					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+						rel="stylesheet"
+					/>
+					<link
+						href="/fonts/tw-icons/css/treinaweb-icons.css"
 						rel="stylesheet"
 					/>
 				</Head>

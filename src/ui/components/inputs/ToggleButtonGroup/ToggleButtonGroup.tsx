b949d9b739535bfe5e49439fba19c5b6acc7a9ab
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-09-15 09:52:13 -0300

    Finalizado componente Grupo de Botões

---
+++ src/ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { } from '@material-ui/core';
+import {
+    ToggleButtonGroupStyled,
+    ToggleButtonStyled,
+} from './ToggleButtonGroup.style';
+
+export interface ToggleButtonGroupProps {}
+
+const ToggleButtonGroup = ToggleButtonGroupStyled;
+
+export const ToggleButton = ToggleButtonStyled;
+
+export default ToggleButtonGroup;

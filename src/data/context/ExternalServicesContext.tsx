de918d4ef92a475e1962393eaa05c41151f3dce3
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-10-04 14:52:29 -0300

    React Context e Reducers - Dados do usuário

---
+++ src/data/context/ExternalServicesContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext } from 'react';
+import {
+    ExternalServiceReducerInterface,
+    initialState,
+    useExternalServicesReducer,
+} from 'data/reducers/ExternalServicesReducers';
+
+const initialValue: ExternalServiceReducerInterface = {
+    externalServicesState: initialState,
+    externalServicesDispatch: () => {},
+};
+export const ExternalServicesContext = createContext({ initialValue });
+
+export const ExternalServicesProvider: React.FC = ({ children }) => {
+    const reducer = useExternalServicesReducer();
+    return (
+        <ExternalServicesContext.Provider value={reducer}>
+            {children}
+        </ExternalServicesContext.Provider>
+    );
+};

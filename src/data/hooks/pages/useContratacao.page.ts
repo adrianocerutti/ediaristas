154d54f7181068ae8adee8c2478e7f12b38170fc
Adriano B. Cerutti <adrianocerutti@gmail.com>
2021-10-07 13:56:28 -0300

    Cálculo do horário de término do serviço

--- src/data/hooks/pages/useContratacao.page.ts
+++ src/data/hooks/pages/useContratacao.page.ts
@@ -1,7 +1,9 @@
-import { FormSchemaService } from 'data/services/FormSchemaService';
-import { useState } from 'react';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { DateService } from './../../services/DateService';
+import { ValidationService } from './../../services/ValidationService';
+import { useState, useMemo, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormSchemaService } from 'data/services/FormSchemaService';
 import {
     CadastroClienteFormDataInterface,
     LoginFormDataInterface,
@@ -10,6 +12,7 @@ import {
 } from 'data/@types/FormInterface';
 import { ServicoInterface } from 'data/@types/ServicoInterface';
 import useApi from '../useApi.hook';
+import { DiariaInterface } from './../../@types/DiariaInterface';
 
 export default function useContratacao() {
     const [step, setStep] = useState(1),
@@ -36,7 +39,41 @@ export default function useContratacao() {
         loginForm = useForm<LoginFormDataInterface>({
             resolver: yupResolver(FormSchemaService.login()),
         }),
-        servicos = useApi<ServicoInterface[]>('/api/servicos').data;
+        servicos = useApi<ServicoInterface[]>('/api/servicos').data,
+        dadosFaxina = serviceForm.watch('faxina'),
+        tipoLimpeza = useMemo<ServicoInterface>(() => {
+            if (servicos && dadosFaxina?.servico) {
+                const selectedService = servicos.find(
+                    (servico) => servico.id === dadosFaxina?.servico
+                );
+                if (selectedService) {
+                    return selectedService;
+                }
+            }
+            return {} as ServicoInterface;
+        }, [servicos, dadosFaxina]),
+        totalTime = useMemo<number>(() => {
+            return calcularTempoServico(dadosFaxina, tipoLimpeza);
+        }, [dadosFaxina, tipoLimpeza]);
+
+    useEffect(() => {
+        if (
+            dadosFaxina &&
+            ValidationService.hora(dadosFaxina.hora_inicio) &&
+            totalTime >= 0
+        ) {
+            serviceForm.setValue(
+                'faxina.hora_termino',
+                DateService.addHours(
+                    dadosFaxina?.hora_inicio as string,
+                    totalTime
+                ),
+                { shouldValidate: true }
+            );
+        } else {
+            serviceForm.setValue('faxina.hora_termino', '');
+        }
+    }, [dadosFaxina?.hora_inicio, totalTime]);
 
     function onServiceFormSubmit(data: NovaDiariaFormDataInterface) {
         console.log(data);
@@ -52,6 +89,25 @@ export default function useContratacao() {
 
     function onPaymentFormSubmit(data: PagamentoFormDataInteface) {
         console.log(data);
+    }
+
+    function calcularTempoServico(
+        dadosFaxina: DiariaInterface,
+        tipoLimpeza: ServicoInterface
+    ) {
+        let total = 0;
+        if (dadosFaxina && tipoLimpeza) {
+            total +=
+                tipoLimpeza.horas_banheiro * dadosFaxina.quantidade_banheiros;
+            total +=
+                tipoLimpeza.horas_cozinha * dadosFaxina.quantidade_cozinhas;
+            total += tipoLimpeza.horas_outros * dadosFaxina.quantidade_outros;
+            total += tipoLimpeza.horas_quarto * dadosFaxina.quantidade_quartos;
+            total +=
+                tipoLimpeza.horas_quintal * dadosFaxina.quantidade_quintais;
+            total += tipoLimpeza.horas_sala * dadosFaxina.quantidade_salas;
+        }
+        return total;
     }
 
     return {
